339f4855be4d2aa86abd933a36cadc030fb1bca6
Michael Winkler <jchillah@gmail.com>
2026-09-08 19:00:47 +0200

    chore(renderer): bump Flutter contract version for record identity

--- Packages/AppForgeKit/Sources/AppForgeApplication/FlutterProjectRenderer.swift
+++ Packages/AppForgeKit/Sources/AppForgeApplication/FlutterProjectRenderer.swift
@@ -51,7 +51,7 @@ public enum FlutterRendererError: Error, Equatable, Sendable {
 }
 
 public struct DeterministicFlutterProjectRenderer: FlutterProjectRendering {
-    public static let rendererVersion = 3
+    public static let rendererVersion = 4
 
     private let specificationValidator: ProjectSpecificationValidator
     private let lockfileBuilder: ForgeLockfileBuilder
